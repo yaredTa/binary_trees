78f704c635c9e1df4c435e94b1c851c6e821da32
yaredTa <yareda6@gmail.com>
2022-11-17 15:42:51 +0300

    binary

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_uncle -  finds the uncle of a node
+  * @node: node of the BT
+  * Return: node of the sibling
+  */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !(node->parent) || !(node->parent->parent))
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+
+	else
+		return (node->parent->parent->left);
+}
